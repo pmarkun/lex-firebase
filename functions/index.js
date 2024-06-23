b4d476396871c800bc7c54f06c39d2a29bab1fca
Luis Leao <luis.leao@gmail.com>
2024-06-23 14:29:07 -0300

    Lex telefonista

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
 
 exports.receiveDonation = require('./receiveDonation').receiveDonation;
 exports.receiveMessage = require('./receiveMessage').receiveMessage;
+exports.receiveCall = require('./receiveCall').receiveCall;
